5f287b230b852802910633c3e84dcbfc7881fb7f
Fernando Morais <fernandomoraisdev@gmail.com>
2023-02-15 10:32:06 -0300

    Funçao para salvar as alteraçoes adicionada

--- script.js
+++ script.js
@@ -1,11 +1,29 @@
 const form = document.querySelector('#form-habits');
 const nlwSetup = new NLWSetup(form)
+const  button =document.querySelector('header button')
 
-const data = {
-    run: ['02-14', '02-15', '02-16', '02-17', '02-18', '02-19', '02-20'],
-    takepills: ['02-14', '02-16'],
-    journal: ['02-15']
+button.addEventListener('click', add)
+form.addEventListener('change', save)
+
+function add() {
+
+    const today = new Date().toLocaleDateString('pt-br').slice(0, -5)
+    const dayExists = nlwSetup.dayExists(today)
+
+    if (dayExists){
+        alert("Dia já existe!❌")
+        return
+        
+    }
+
+    alert('Adicionado com sucesso! ✅')
+    nlwSetup.addDay(today)
 }
 
+function save() {
+    localStorage.setItem('NLWSetup@habits', JSON.stringify(nlwSetup.data))
+}
+
+const data = JSON.parse(localStorage.getItem('NLWSetup@habits')) || {}
 nlwSetup.setData(data)
 nlwSetup.load()
